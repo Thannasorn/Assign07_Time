c7bc4687de05174ad955687f313b40d880427ade
Thannasorn Metta <vk0831671865@gmail.com>
2022-04-11 22:23:25 +0700

    add

---
+++ app/src/main/java/com/example/assign07time6206021611125/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.assign07time6206021611125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
